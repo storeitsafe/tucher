6cffaf6012fd9ce00427768256c9b24b344590e5
Jagjeet Singh <support@seasonalcoder.com>
2022-07-25 20:23:40 +0530

    Added function to create menu pages

--- themes/tucher/inc/admin/classes.tucher.settings.php
+++ themes/tucher/inc/admin/classes.tucher.settings.php
@@ -6,8 +6,28 @@ if(!class_exists('theme_settings')):
 
         public function initalize(){
 
-            echo  "function called";
-        }
-    }
+            $current = 'homepage';
 
+                $tabs = array( 'homepage' => 'Home Settings', 'general' => 'General', 'footer' => 'Footer' );
+                echo '<div id="icon-themes" class="icon32"><br></div>';
+                echo '<h2 class="nav-tab-wrapper">';
+                foreach( $tabs as $tab => $name ){
+                    $class = ( $tab == $current ) ? ' nav-tab-active' : '';
+                    echo "<a class='nav-tab$class' href='?page=theme-settings&tab=$tab'>$name</a>";
+
+                }
+                echo '</h2>';
+
+                 $this->tucher_settings_page();
+             }
+
+                   
+
+            function tucher_settings_page() {
+
+                global $pagenow;
+                $settings = get_option( "tucher_theme_settings" );
+
+            }
+    }
 endif;
